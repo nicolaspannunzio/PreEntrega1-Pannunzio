aac0b721cd954b3024acb96d7beba47bca158e44
Nicolas Pannunzio <nicolas.a.pannunzio@gmail.com>
2023-08-22 16:37:10 -0300

    agregando imágenes, sumando JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,61 @@
-// 1- crear un algoritmo c/un condicional
-// 2- crar un algortimo utilizando un ciclo
+//1- Condicionales en JS - Estructura: IF.. ELSE
+
+// let collar = true;
+// if (collar) {
+//     console.log("usalo para pasear a tu mascota");
+// }
+
+// let tipo = prompt("Que clase de collar prefieres? correa corta o correa larga");
+
+// if (tipo == "correa corta"){
+//     console.log("Ok, Ud se lleva una corra corta");
+// } else if (tipo == "correa larga") {
+//     console.log("Ok, Ud se lleva una correa larga");
+// } else {
+//     console.log(prompt("Repita su respuesta: correa corta o correa larga"));
+// }
+
+// let compra1 = prompt("Digame que articulo desea");
+// let compra2 = prompt("Cual es su medio de pago preferido?: efectivo o crédito");
+
+// if ((compra1 != "") && (compra2 != "")){
+//     alert("Su producto elegido " + compra1 + " y su medio de pago es " +compra2)
+// } else{
+//     alert("Operación inválida, inténtelo de nuevo");
+// }
+
+// let mascotaPreferida = prompt("Digame cual es su mascota preferida");
+
+// switch (mascotaPreferida){
+//     case "perro":
+//         console.log("Su mascota preferida son los perros");
+//         break;
+//     case "gato":
+//         console.log("Su mascotta preferida son los gatos"); 
+//         break;
+//     case "peces":
+//         console.log("Su mascota preferida son los peces");
+//         break;
+//     case "aves":
+//         console.log("Su mascotta preferida son los aves"); 
+//         break;
+//     default:
+//         console.log("Vuelva a intentarlo");
+//         break;
+// }
+
+// // 2- crar un algortimo utilizando un ciclo: for, while, do while
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("Dime un número menor a cinco");
+// }
+
+
+
+
+
+
+
 // 3- para los anteriores, utilizar funciones
 // 4- armar un simulador
 // 5- armar la estructura del html c/ etiqueta JS
-
-//1- Condicionales en JS - Estructura: IF.. ELSE
-
-let collar = true;
-if (collar) {
-    console.log("usalo para pasear a tu mascota");
-}
-
-let tipo = prompt("Que clase de collar prefieres? correa corta o correa larga");
-
-if (tipo == "correa corta"){
-    console.log("Ok, Ud se lleva una corra corta");
-} else if (tipo == "correa larga") {
-    console.log("Ok, Ud se lleva una correa larga");
-} else {
-    console.log(prompt("Repita su respuesta: correa corta o correa larga"));
-}
-
-let compra1 = prompt("Digame que articulo desea");
-let compra2 = prompt("Cual es su medio de pago preferido?: efectivo o crédito");
-
-if ((compra1 != "") && (compra2 != "")){
-    alert("Su producto elegido " + compra1 + " y su medio de pago es " +compra2)
-} else{
-    alert("Operación inválida, inténtelo de nuevo");
-}
-
-let mascotaPreferida = prompt("Digame cual es su mascota preferida");
-
-switch (mascotaPreferida){
-    case perro:
-        console.log("Su mascota preferida son los perros");
-        break;
-    case gato:
-        console.log("Su mascotta preferida son los gatos"); 
-        break;
-    case peces:
-        console.log("Su mascota preferida son los peces");
-        break;
-    case aves:
-        console.log("Su mascotta preferida son los aves"); 
-        break;
-    default:
-        console.log("Vuelva a intentarlo");
-        break;
-}
