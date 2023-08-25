9018b89030a5b3d01d39b37a4cc58ddf63b6ef12
Nicolas Pannunzio <nicolas.a.pannunzio@gmail.com>
2023-08-25 12:42:50 -0300

    modificaciones en html, y primeras funcionalidades JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,61 +1,18 @@
-//1- Condicionales en JS - Estructura: IF.. ELSE
+/* Saludo de bienvenida */
 
-// let collar = true;
-// if (collar) {
-//     console.log("usalo para pasear a tu mascota");
-// }
+//alert("¡Bienvenidxs a nuestra tienda de mascotas, M&T!");
 
-// let tipo = prompt("Que clase de collar prefieres? correa corta o correa larga");
+/* Inicio del Simulador. Comprobando si el usuario está registrado o no. */
 
-// if (tipo == "correa corta"){
-//     console.log("Ok, Ud se lleva una corra corta");
-// } else if (tipo == "correa larga") {
-//     console.log("Ok, Ud se lleva una correa larga");
-// } else {
-//     console.log(prompt("Repita su respuesta: correa corta o correa larga"));
-// }
+let usuario = prompt("Hola! ¿Tenes un usuario registrado?");
+let nuevoUsuario = "";
 
-// let compra1 = prompt("Digame que articulo desea");
-// let compra2 = prompt("Cual es su medio de pago preferido?: efectivo o crédito");
-
-// if ((compra1 != "") && (compra2 != "")){
-//     alert("Su producto elegido " + compra1 + " y su medio de pago es " +compra2)
-// } else{
-//     alert("Operación inválida, inténtelo de nuevo");
-// }
-
-// let mascotaPreferida = prompt("Digame cual es su mascota preferida");
-
-// switch (mascotaPreferida){
-//     case "perro":
-//         console.log("Su mascota preferida son los perros");
-//         break;
-//     case "gato":
-//         console.log("Su mascotta preferida son los gatos"); 
-//         break;
-//     case "peces":
-//         console.log("Su mascota preferida son los peces");
-//         break;
-//     case "aves":
-//         console.log("Su mascotta preferida son los aves"); 
-//         break;
-//     default:
-//         console.log("Vuelva a intentarlo");
-//         break;
-// }
-
-// // 2- crar un algortimo utilizando un ciclo: for, while, do while
-
-// for (let i = 0; i < 5; i++) {
-//     console.log("Dime un número menor a cinco");
-// }
-
-
-
-
-
-
-
-// 3- para los anteriores, utilizar funciones
-// 4- armar un simulador
-// 5- armar la estructura del html c/ etiqueta JS
+if (usuario === "si" || usuario === "Si"){
+    console.log("Ok!");
+} else if (usuario === "no" || usuario === "No"){
+    prompt("Ok, queres registrarte?");
+} else if (nuevoUsuario === "si" || nuevoUsuario === "Si"){
+    nuevoUsuario = prompt("Por favor, elegí tu nombre de usuario. El mismo debe tener al menos 8 caracteres");
+} else {
+    alert("intentalo de nuevo");
+}
